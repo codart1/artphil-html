b1f515563b8bc943e849a3e9d7536644d08b66ad
nnt25251325 <nguyennhutinh18@gmail.com>
2015-07-02 08:49:03 +0700

    dgsdg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 ! function($) {
   $(document).ready(function() {
     /* --------------------------------------------------------------------- */
-    /* .slider-main
+    /* #caroufredsel-slider-main
     /* --------------------------------------------------------------------- */
     (function() {
-      if ($('.slider-main').length) {
+      if ($('#caroufredsel-slider-main').length) {
         function runSliderMain() {
           $('#caroufredsel-slider-main .sliders').carouFredSel({
             infinite: true,
@@ -79,43 +79,94 @@
       });
     })();
 
-  });
 
 
-  /* --------------------------------------------------------------------- */
-  /* .anchor smooth scroll
-  /* --------------------------------------------------------------------- */
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
+    /* --------------------------------------------------------------------- */
+    /* .anchor smooth scroll
+    /* --------------------------------------------------------------------- */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
         }
-      }
-    });
-  });
-
-
-  /* --------------------------------------------------------------------- */
-  /* .fixed snb
-  /* --------------------------------------------------------------------- */
-  $(function() {
-    var gnb = $('#gnb');
-    var snb = $('#snb');
-
-    $(window).scroll(function() {
-      if (gnb.visible(true)) {
-        snb.removeClass('fixed');
-      } else {
-        snb.addClass('fixed');
-      }
+      });
     });
 
-  });
 
+    /* --------------------------------------------------------------------- */
+    /* .fixed snb
+    /* --------------------------------------------------------------------- */
+    $(function() {
+      var gnb = $('#gnb');
+      var snb = $('#snb');
+
+      $(window).scroll(function() {
+        if (gnb.visible(true)) {
+          snb.removeClass('fixed');
+        } else {
+          snb.addClass('fixed');
+        }
+      });
+    });
+
+
+
+    /* --------------------------------------------------------------------- */
+    /* #caroufredsel-slider-special-courses
+    /* --------------------------------------------------------------------- */
+    (function() {
+      if ($('#caroufredsel-slider-special-courses').length) {
+        function runSliderMain() {
+          $('#caroufredsel-slider-special-courses .sliders').carouFredSel({
+            infinite: true,
+            circular: true,
+            responsive: true,
+            debug: false,
+            items: {
+              start: 0
+            },
+
+            scroll: {
+              items: 1,
+              duration: 600,
+              fx: "scroll"
+            },
+
+            auto: {
+              timeoutDuration: 6000,
+              play: true
+            },
+
+            prev: {
+              button: "#caroufredsel-slider-special-courses-prev"
+            },
+
+            next: {
+              button: "#caroufredsel-slider-special-courses-next"
+            },
+
+            pagination: {
+              container: "#caroufredsel-slider-special-courses-pagination"
+            },
+
+            swipe: {
+              onTouch: true,
+              onMouse: true
+            }
+          });
+        }
+        $("#caroufredsel-slider-special-courses").imagesLoaded(runSliderMain);
+      }
+    })();
+
+
+
+  });
 }(jQuery);
